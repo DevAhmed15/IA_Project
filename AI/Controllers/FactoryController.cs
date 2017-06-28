4ee1c77557f96ffa5757e17b9396f051e1e52747
Developer Ahmed <ahmed.fcih1@hotmail.com>
2017-06-28 21:19:26 +0200

    IA_project
    asp.net codefirst project For Internet application course

---
+++ AI/Controllers/FactoryController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AI.Controllers
+{
+    public class FactoryController : Controller
+    {
+        //
+        // GET: /Factory/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult admEditor()
+        {
+            return View();
+        }
+        public ActionResult admNews()
+        {
+            ViewBag.Message = "Admin section for news Edition & Deletion.";
+            ViewBag.editor1_p = "“Nullam dapibus blandit orci, viverra gravida dui lobortis eget. Maecenas fringilla urna eu nisl scelerisque.”";
+            ViewBag.editor1_name = "Chanel Iman";
+            ViewBag.editor2_p = "“Nullam dapibus blandit orci, viverra gravida dui lobortis eget. Maecenas fringilla urna eu nisl scelerisque.”";
+            ViewBag.editor2_name = "Chanel Iman";
+            ViewBag.editor3_p = "“Nullam dapibus blandit orci, viverra gravida dui lobortis eget. Maecenas fringilla urna eu nisl scelerisque.”";
+            ViewBag.editor3_name = "Chanel Iman";
+            ViewBag.editor4_p = "“Nullam dapibus blandit orci, viverra gravida dui lobortis eget. Maecenas fringilla urna eu nisl scelerisque.”";
+            ViewBag.editor4_name = "Chanel Iman";
+            return View();
+        }
+        public ActionResult admEditedNews()
+        {
+            return View();
+        }
+        public ActionResult EdiNews()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ AI/Controllers/WallController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AI.Controllers
+{
+    public class WallController : Controller
+    {
+        //
+        // GET: /Wall/
+
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        public ActionResult news()
+        {
+            return View();
+        }
+    }
+}
